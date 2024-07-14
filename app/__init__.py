044772b11a11c0d3bdd7adc0cfa5158877a38f8c
Ramez Salman <ramezsalman@gmail.com>
2024-07-14 17:55:14 +0500

    Added an initial setup for database and routes

---
+++ app/__init__.py
@@ -0,0 +1,48 @@
+import psycopg2
+from dotenv import load_dotenv
+from flask import Flask, g
+from .config import Config
+from .routes import routes
+
+load_dotenv()
+ 
+def connect_db(logger):
+    try:
+        connection = psycopg2.connect(
+            database=Config.DB_NAME,
+            user=Config.DB_USER,
+            password=Config.DB_PASSWORD,
+            host=Config.DB_HOST,
+            port=Config.DB_PORT
+        )
+        return connection
+    except psycopg2.OperationalError as e:
+        logger.error(f"OperationalError: {e}")
+    except psycopg2.InterfaceError as e:
+        logger.error(f"InterfaceError: {e}")
+    except psycopg2.DatabaseError as e:
+        logger.error(f"DatabaseError: {e}")
+    except Exception as e:
+        logger.error(f"Error: {e}")
+
+    return None
+
+def close_db(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    @app.before_request
+    def before_request():
+        g.db = connect_db(app.logger)
+
+    @app.teardown_appcontext
+    def teardown_db(exception):
+        close_db()
+
+    app.register_blueprint(routes)
+    return app
